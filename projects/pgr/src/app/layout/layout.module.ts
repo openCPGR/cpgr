5ac28d30f26e38ef6bbd6e51a787971172edddee
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2023-03-26 18:38:59 +0530

    layout added

--- projects/pgr/src/app/layout/layout.module.ts
+++ projects/pgr/src/app/layout/layout.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule
+  ],
+  exports:[LayoutComponent]
 })
 export class LayoutModule { }
