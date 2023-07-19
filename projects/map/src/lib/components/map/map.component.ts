010bf4d71894682b225bddca031ea5d5e8c752c2
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2023-07-20 00:23:32 +0530

    map

--- projects/map/src/lib/components/map/map.component.ts
+++ projects/map/src/lib/components/map/map.component.ts
@@ -12,9 +12,43 @@ export class MapComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    var map = L.map('map', { zoomSnap: 0.25 }).setView([22, 77], 4.5);
-    L.tileLayer('https://mappls.com/atms/near/28.5454,77.45445,2').addTo(map);
-    map.addLayer(new CanvasLayer());
+    // var map = L.map('map', { zoomSnap: 0.25 }).setView([22, 77], 4.5);
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    // }).addTo(map);
+
+    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    });
+
+    const osmHOT = L.tileLayer(
+      'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France',
+      }
+    );
+    var openTopoMap = L.tileLayer(
+      'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)',
+      }
+    );
+    var baseMaps = {
+      OpenStreetMap: osm,
+      'OpenStreetMap.HOT': osmHOT,
+      openTopoMap: openTopoMap,
+    };
+    var map = L.map('map', { zoomSnap: 0.25, layers: [osm] }).setView(
+      [22, 77],
+      4.5
+    );
+    var layerControl = L.control.layers(baseMaps).addTo(map);
     console.log(map);
   }
 }
